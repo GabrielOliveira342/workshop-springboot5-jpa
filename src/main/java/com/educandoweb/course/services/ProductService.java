d0c3e2adfd1850936a852727e198e46887d227f7
Gabriel Alberto de Oliveira <gabrielalbertodeoliveira17@gmail.com>
2026-02-12 23:59:26 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/educandoweb/course/services/ProductService.java
+++ src/main/java/com/educandoweb/course/services/ProductService.java
@@ -1,7 +1,6 @@
 package com.educandoweb.course.services;
 
 import java.util.List;
-
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,11 +14,11 @@ public class ProductService {
 
 	@Autowired
 	private ProductRepository repository;
-	
-	public List<Product> findAll(){
+
+	public List<Product> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public Product findById(Long id) {
 		Optional<Product> obj = repository.findById(id);
 		return obj.get();
